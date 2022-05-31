160b5d5bb64086a9f8715f3f4c569d2554d9ed38
Clifforddd <cliffhuang91@gmail.com>
2022-05-31 13:12:02 -0700

    final version

--- reduce.js
+++ reduce.js
@@ -6,7 +6,35 @@ Examples:
     extractValue(arr,'name') // ['Elie', 'Tim', 'Matt', 'Colt']
 */
 
-function extractValue(arr, key) {}
+function extractValue(arr, key) {
+    return arr.reduce(function(accum, nextName) {
+         accum.push(nextName[key]); //why cant use nextName.key, any diffrence?
+        return accum
+    }, []); //why using [] here
+}
+
+
+// Write a function called `findUserByUsername` which accepts an array of objects, each with a key of username, and a string. The function should return the first object with the key of username that matches the string passed to the function. If the object is not found, return undefined. 
+
+// const users = [
+//   {username: 'mlewis'},
+//   {username: 'akagen'},
+//   {username: 'msmith'}
+// ];
+
+// findUserByUsername(users, 'mlewis') // {username: 'mlewis'}
+// findUserByUsername(users, 'taco') // undefined
+// */
+
+
+// function findUserByUsername(usersArray, username) {
+//   return usersArray.find(function(uName) {
+
+//      return uName.username === username;  //why cant use uName[username.toString] === username;
+    
+//   })
+// }
+
 
 /*
 Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
@@ -19,7 +47,23 @@ Examples:
     vowelCount('I Am awesome and so are you') // {i: 1, a: 4, e: 3, o: 3, u: 1};
 */
 
-function vowelCount(str) {}
+function vowelCount(str) {
+    str = str.toLowerCase().split("");
+    const vowel = "aeiou";
+    
+    return str.reduce(function(accum, nextChar) {
+        let lowerC = nextChar.toLowerCase();
+        if(vowel.indexOf(lowerC) !== -1) {
+            if(accum[lowerC]) {
+                accum[lowerC]++;
+            }
+            else{
+                accum[lowerC] = 1;
+            }
+        } 
+        return accum;
+    }, {});
+}
 
 /*
 Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
@@ -36,12 +80,17 @@ Examples:
        ]
 */
 
-function addKeyAndValue(arr, key, value) {}
+function addKeyAndValue(arr, key, value) {
+    return arr.reduce(function(accum, nextVal, index1) { // why index1 here, no nextVal?
+        accum[index1][key] = value;
+        return accum;
+    }, arr);
+}
 
 /*
 Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
 
-Examples:
+Examples: 
     
     function isEven(val){
         return val % 2 === 0;
@@ -60,4 +109,13 @@ Examples:
     partition(names, isLongerThanThreeCharacters) // [['Elie', 'Colt', 'Matt'], ['Tim']]
 */
 
-function partition(arr, callback) {}
+function partition(arr, callback) {
+    return arr.reduce(function(accum,nextName){
+        if(callback(nextName)){
+            accum[0].push(nextName);
+        } else {
+            accum[1].push(nextName);
+        }
+        return accum;
+    }, [[],[]]); 
+}
